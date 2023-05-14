2d72558ff45392355be5f55cd42a58f67c8fa494
Thoss24 <t.fogarty24@outlook.com>
2023-05-14 13:23:08 +0100

    cart modal display styling added

--- src/components/Modal/Cart.js
+++ src/components/Modal/Cart.js
@@ -1,13 +1,20 @@
 import classes from "./Cart.module.css";
 import CartItem from "./CartItem";
+import React, { Fragment } from 'react'
 
 const Cart = (props) => {
   return (
+    <Fragment>
+    <div className={classes.backdrop}/>
     <div className={classes['cart-container']}>
+    <div className={classes['cart-items-container']}>
         {props.cartItems.map((item) => (
             <CartItem price={item.price} name={item.name} amount={item.amount} key={item.key}/>
         ))}
     </div>
+    <button>Order</button>
+    </div>
+    </Fragment>
   );
 };
 
